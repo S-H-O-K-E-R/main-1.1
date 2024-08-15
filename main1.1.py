1acdbd7fe34040847f5555a15fcf636280576722
S-H-O-K-E-R <kolyan482650@gmail.com>
2024-08-15 17:18:53 +0500

    Add files via upload

---
+++ main1.1.py
@@ -0,0 +1,6 @@
+completed_tasks = 12
+amount_time = 1.5
+course = 'Python'
+time_for_one_task = amount_time/completed_tasks
+
+print('Курс: ',course,', всего задач:',completed_tasks,', затрачено часов:',amount_time,', среднее время выполнения ',time_for_one_task,' часа.')
